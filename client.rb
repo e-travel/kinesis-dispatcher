baf04fad1c6e7cf80ff998a4d95501e064149452
Kyriakos Kentzoglanakis <k.kentzoglanakis@pamediakopes.gr>
2017-11-06 10:33:57 +0200

    Adds support for tcp and uds in server and client

--- client.rb
+++ client.rb
@@ -1,3 +1,25 @@
 require 'socket'
 
-s = TCPSocket.new 'localhost', 8888
+type = ARGV.last
+if type.nil?
+  puts("please specify unix or tcp")
+  exit(1)
+end
+
+puts "Each new line is a new message. C-d to finish."
+
+socket =
+  case type
+  when 'unix'
+    UNIXSocket.new '/tmp/kinesis-dispatcher.sock'
+  when 'tcp'
+    TCPSocket.new 'localhost', 8888
+  else
+    raise "type %s not supported"
+  end
+
+while (msg = STDIN.gets)
+  socket.write(msg)
+end
+
+socket.close
--- main.go
+++ main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"log"
 	"net"
+	"os"
 )
 
+func echo(conn io.Reader) {
+	io.Copy(os.Stdout, conn)
+}
+
 func main() {
-	listener, err := net.Listen("tcp", ":8888")
+	if len(os.Args) != 2 {
+		fmt.Println("Please specify unix or tcp")
+		os.Exit(1)
+	}
+
+	var listener net.Listener
+	var err error
+	switch os.Args[1] {
+	case "tcp":
+		listener, err = net.Listen("tcp", ":8888")
+	case "unix":
+		os.Remove("/tmp/kinesis-dispatcher.sock")
+		listener, err = net.Listen("unix", "/tmp/kinesis-dispatcher.sock")
+	default:
+		fmt.Println("Invalid type specified.")
+		os.Exit(1)
+	}
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -20,11 +41,11 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		go func(conn net.Conn) {
-			var buf bytes.Buffer
-			io.Copy(&buf, conn)
-			fmt.Println(buf)
+		go func(conn net.Conn, handler func(io.Reader)) {
+			fmt.Println("Handling connection")
+			handler(conn)
+			fmt.Println("Closing connection")
 			conn.Close()
-		}(conn)
+		}(conn, echo)
 	}
 }
