de38c3ec27d3a1c8d431e26ade5c8c408970b7b7
Kyriakos Kentzoglanakis <k.kentzoglanakis@pamediakopes.gr>
2017-11-17 17:07:49 +0200

    Uses mock client in buffered dispatcher's tests

--- buffered_dispatcher_test.go
+++ buffered_dispatcher_test.go
@@ -6,11 +6,17 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type MockClient struct{}
+
+func (client *MockClient) Put(data [][]byte) bool {
+	return true
+}
+
 func TestPutPlacesMessageInQueue(t *testing.T) {
 	config := &Config{
 		bufferSize: 1,
 	}
-	dispatcher := NewBufferedDispatcher(config, &KinesisClient{})
+	dispatcher := NewBufferedDispatcher(config, &MockClient{})
 	assert.True(t, dispatcher.Put([]byte("hello")))
 	assert.Equal(t, []byte("hello"), <-dispatcher.queue)
 }
@@ -19,7 +25,7 @@ func TestDropsMessageWhenQueueIsFull(t *testing.T) {
 	config := &Config{
 		bufferSize: 1,
 	}
-	dispatcher := NewBufferedDispatcher(config, &KinesisClient{})
+	dispatcher := NewBufferedDispatcher(config, &MockClient{})
 	dispatcher.Put([]byte("hello"))
 	assert.False(t, dispatcher.Put([]byte("goodbye")))
 	assert.Equal(t, []byte("hello"), <-dispatcher.queue)
