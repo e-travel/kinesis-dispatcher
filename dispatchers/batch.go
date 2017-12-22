2b0d4256b93fd06503f79f07f85fbf14e1e45d4f
Kyriakos Kentzoglanakis <kyriakos.kentzoglanakis@gmail.com>
2017-12-22 16:19:35 +0200

    Removes unused batch type

--- dispatchers/batch.go
+++ dispatchers/batch.go
@@ -12,28 +12,3 @@ type Batch interface {
 	CanAdd(message []byte) bool
 	Len() int
 }
-
-// =============================
-// A mock Batch type for testing
-type SimpleBatch struct {
-	Messages []string
-}
-
-func NewSimpleBatch() *SimpleBatch {
-	return &SimpleBatch{
-		Messages: make([]string, 10),
-	}
-}
-
-func (b *SimpleBatch) Add(message []byte) error {
-	b.Messages = append(b.Messages, string(message))
-	return nil
-}
-
-func (b *SimpleBatch) CanAdd(message []byte) bool {
-	return true
-}
-
-func (b *SimpleBatch) Len() int {
-	return len(b.Messages)
-}
